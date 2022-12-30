959860bf264bb814792139ad8ee4ec0d5a9f1917
ishwariya nanjundan <ishwariyananjundan@ishwariyas-MacBook-Pro.local>
2022-12-29 23:50:30 -0800

    Updates

---
+++ precache-manifest.1b85486ff7e4a5c4aa8415b293a2547a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3f4ec8954b0bc0c3b32ba1e95cb888",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "9e9afe427c866270395e",
+    "url": "/personal-website/static/css/2.5b5e1d1a.chunk.css"
+  },
+  {
+    "revision": "76596d2a6cf7f33b6a73",
+    "url": "/personal-website/static/css/main.9d4e06fe.chunk.css"
+  },
+  {
+    "revision": "9e9afe427c866270395e",
+    "url": "/personal-website/static/js/2.55f9b121.chunk.js"
+  },
+  {
+    "revision": "ffbce54d9044ae393f20a507b643713f",
+    "url": "/personal-website/static/js/2.55f9b121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76596d2a6cf7f33b6a73",
+    "url": "/personal-website/static/js/main.59dcd790.chunk.js"
+  },
+  {
+    "revision": "db90418ad44df9c153f0",
+    "url": "/personal-website/static/js/runtime-main.6e0eea24.js"
+  },
+  {
+    "revision": "f6c1998b7a2c4dbe763eab186edbd850",
+    "url": "/personal-website/static/media/Sucheth_Resume.f6c1998b.pdf"
+  },
+  {
+    "revision": "336641f2909845e78c8e8ed8e56df5d0",
+    "url": "/personal-website/static/media/badminton.336641f2.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/personal-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/personal-website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/personal-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/personal-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/personal-website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c13d139b1f70d28323f700eef6cf2286",
+    "url": "/personal-website/static/media/weeurls.c13d139b.jpg"
+  }
+]);
